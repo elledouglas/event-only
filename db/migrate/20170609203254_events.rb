ca999956c19ca60fb6cbc0f7cf83dea6dd5dbd1b
Kathleen <kathleenmcclay@gmail.com>
2017-06-09 20:27:36 -0400

    Event commit

---
+++ db/migrate/20170609203254_events.rb
@@ -0,0 +1,4 @@
+class Events < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302213154) do
+ActiveRecord::Schema.define(version: 20170609203254) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
